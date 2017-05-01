01546302341049027d61818dac8c38184bdd9656
John Ballon <johnballon@Johns-MacBook-Pro.local>
2017-04-30 17:06:41 -0700

    Begin JS exam review

---
+++ a05_problems_master/problems/binarySearch.js
@@ -0,0 +1,7 @@
+function myBsearch(array, target) {
+
+}
+
+console.log(myBsearch([1,2,3,4,5], 2) ); // 1
+console.log(myBsearch([1,2,3,4,5], 5)); // 4
+console.log(myBsearch([1,2,3,4,5], 6)); // -1
